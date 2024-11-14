e39c81a74e48315dea81002797643a716b99e320
Alena Klekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2024-11-14 20:27:44 +0100

    Update main.py
    half part through

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ msg_6 = " ... lazy"
 msg_7 = " ... very lazy"
 msg_8 = " ... very, very lazy"
 msg_9 = "You are"
+msg_10 = "Are you sure? It is only one digit! (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
 
 
 def calculate_result(x, op, y):
@@ -43,66 +46,79 @@ def get_x_y_oper(memory):
         x = calc[0]
         oper = calc[1]
         y = calc[2]
-
         if x == 'M':
             x = memory
         if y == 'M':
             y = memory
+        if x != 'M' and y != 'M':
+            memory = 0
         operations = ['+', '-', '*', '/']
         try:
             x = float(x)
             y = float(y)
             check_result = check(x, y, oper)
             if oper not in operations:
-                print(check_result)
+                if len(check_result) != 0:
+                    print(check_result)
                 print(msg_2)
             elif oper == '/' and y == 0:
-                print(check_result)
+                if len(check_result) != 0:
+                    print(check_result)
                 print(msg_3)
             else:
-                print(check_result)
+                if len(check_result) != 0:
+                    print(check_result)
                 return x, oper, y
         except:
-            print(check_result)
             print(msg_1)
 
 
-def result_storage(details):
-    print(msg_4)
+def messages_creator(msg_index):
+    message = "msg_{}".format(msg_index)
+    if message == 'msg_10':
+        return msg_10
+    elif message == 'msg_11':
+        return msg_11
+    elif message == 'msg_12':
+        return msg_12
+
+
+def message_four(msg_index, memory):
+    details = get_x_y_oper(memory)
+    calculated_result = calculate_result(details[0], details[1], details[2])
+    print(calculated_result)
     #"Do you want to store the result? (y / n):"
+    print(msg_4)
     answer = input()
     if answer == 'y':
-        memory = calculate_result(details[0], details[1], details[2])
-        print(msg_5)
-        #"Do you want to continue calculations? (y / n):"
-        second_answer = input()
-        if second_answer == 'y':
-            details = get_x_y_oper(memory)
-            return calculate_result(details[0], details[1], details[2])
-        if second_answer == 'n':
-            return 'n'
+        if -10 < calculated_result < 10 and calculated_result.is_integer():
+            while msg_index <= 12:
+                m = messages_creator(msg_index)
+                print(m)
+                ans = input()
+                if ans == 'y':
+                    msg_index += 1
+                    if msg_index == 13:
+                        message_five(calculated_result)
+                if ans == 'n':
+                    break
+        else:
+            return calculated_result
     if answer == 'n':
+        message_five(0)
+
+
+def message_five(memory):
+    while True:
         print(msg_5)
-        memory = 0
-        #"Do you want to continue calculations? (y / n):"
-        second_answer = input()
-        if second_answer == 'y':
-            details = get_x_y_oper(memory)
-            result = calculate_result(details[0], details[1], details[2])
+        answer = input()
+        if answer == 'y':
+            result = message_four(msg_index, memory)
             return result
-        if second_answer == 'n':
-                return 'n'
+        if answer == 'n':
+            break
 
 
-memory = 0
-details = get_x_y_oper(memory)
-if len(details) == 3:
-    print(calculate_result(details[0], details[1], details[2]))
-else:
-    print(details)
+msg_index = 10
 while True:
-    memory = result_storage(details)
-    if memory == 'n':
-        break
-    else:
-        print(memory)
+    memory = message_four(msg_index, 0)
